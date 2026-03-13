eb7aedd83e50dd8931b65859831bd4bb96043d6c
Bhavya Agrawal <agrawalbhavya943@gmail.com>
2026-03-13 19:35:12 +0530

    Multiple UI improvements and fixes in submission and committee pages

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,10 +14,23 @@ const publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
+/* Homepage */
 app.get("/", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
+/* Clean URL routing */
+app.get("/:page", (req, res) => {
+  const page = req.params.page;
+  const filePath = path.join(publicPath, `${page}.html`);
+
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).send("Page not found");
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
